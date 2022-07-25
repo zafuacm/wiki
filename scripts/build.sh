6848412afb93c4fe8f24e9741d03cdfbb133ab08
rogeryoungh <rogeryoungh@foxmail.com>
2022-07-26 00:04:55 +0800

    fix: couldn't found preprocessor

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-curl -sSL https://github.com/rust-lang/mdBook/releases/download/v0.4.21/mdbook-v0.4.21-x86_64-unknown-linux-gnu.tar.gz | tar -xz
-curl -sSL https://github.com/tommilligan/mdbook-admonish/releases/download/v1.7.0/mdbook-admonish-v1.7.0-x86_64-unknown-linux-gnu.tar.gz | tar -xz
-wget -q https://github.com/rogeryoungh/preprocessor-md-tex/releases/download/v0.1.2/preprocessor-md-tex
+mkdir -p $HOME/bin
 
-chmod +x mdbook
-chmod +x mdbook-admonish
-chmod +x preprocessor-md-tex
+curl -sSL https://github.com/rust-lang/mdBook/releases/download/v0.4.21/mdbook-v0.4.21-x86_64-unknown-linux-gnu.tar.gz | tar -xz --directory=$HOME/bin
+curl -sSL https://github.com/tommilligan/mdbook-admonish/releases/download/v1.7.0/mdbook-admonish-v1.7.0-x86_64-unknown-linux-gnu.tar.gz | tar -xz --directory=$HOME/bin
+wget -q https://github.com/rogeryoungh/preprocessor-md-tex/releases/download/v0.1.2/preprocessor-md-tex -O $HOME/bin/preprocessor-md-tex
 
-./preprocessor-md-tex ./docs/
-./mdbook build
+chmod +x $HOME/bin/mdbook
+chmod +x $HOME/bin/mdbook-admonish
+chmod +x $HOME/bin/preprocessor-md-tex
+
+export PATH="$HOME/bin:$PATH$
+
+preprocessor-md-tex ./docs/
+mdbook build
