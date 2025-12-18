fe8c3cf7fdbdbcfbd0c7d726520d7a77649ad450
xdcccccccccccc <xdcccccccccccc@users.noreply.github.com>
2025-12-18 07:34:53 +0000

    deploy: 823d1d667d689eb0ec6c79be8eab5cd63bcb7795

--- assets/other_english.md.Cn2ji73Q.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as a}from"./chunks/framework.B_6h9_5G.js";const h=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"other/english.md","filePath":"other/english.md","lastUpdated":1766042695000}'),r={name:"other/english.md"};function s(o,n,c,i,d,l){return a(),t("div")}const _=e(r,[["render",s]]);export{h as __pageData,_ as default};
