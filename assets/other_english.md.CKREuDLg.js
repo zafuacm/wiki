2d70cb78c499c723c228135e9c683089099c3feb
xdcccccccccccc <xdcccccccccccc@users.noreply.github.com>
2026-01-01 06:20:18 +0000

    deploy: 8265c03cabbabccfce6af0f0ebaab8f160835792

--- assets/other_english.md.CKREuDLg.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as a}from"./chunks/framework.ZDgf3h-8.js";const h=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"other/english.md","filePath":"other/english.md","lastUpdated":1767248272000}'),r={name:"other/english.md"};function s(o,n,c,i,d,l){return a(),t("div")}const _=e(r,[["render",s]]);export{h as __pageData,_ as default};
